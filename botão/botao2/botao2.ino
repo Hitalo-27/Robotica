73f2543cde98855f0b28de540352d44765a8bf9c
Hitalo chaves <56124084+Hitalo-27@users.noreply.github.com>
2020-09-30 20:36:42 -0300

    Add files via upload

---
+++ botão/botao2/botao2.ino
@@ -0,0 +1,23 @@
+
+int botao = 8;
+int estadoBotao = 0;
+int led = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (botao, INPUT);
+pinMode (led, OUTPUT); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+estadoBotao = digitalRead (botao);
+if (estadoBotao == HIGH){
+  digitalWrite (led, HIGH);
+}
+
+else{ 
+  digitalWrite (led, LOW);
+}
+}
